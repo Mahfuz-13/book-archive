c544a39edcba1d91b2d55f7d118b215807f9095b
Mahfuz-13 <mahfuzulh14@gmail.com>
2021-09-02 10:14:10 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// Fecth the data after clicking the button
+const searchBook = ()=>{
+    const input = document.getElementById('input');
+    const inputValue = input.value;
+    console.log(inputValue);
+    // Clear Input Box
+    input.value = '';
+}
